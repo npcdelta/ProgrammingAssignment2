31a111e9083c6b0aeb0671393923886484ebb4f5
npcdelta <npcdelta@gmail.com>
2015-09-26 17:08:47 -0500

    Revert "ProgrammingAssignment2"
    This reverts commit 188c7ea8489964767cdbb99a1401b86409522422.

--- cachematrix.R
+++ cachematrix.R
@@ -1,42 +1,15 @@
-## When used in conjunction, the two functions below will
-## return the cached value of the inverse of a matrix if it
-## has previously been calculated. Otherwise, it will calculate
-## the inverse and cache the result for future use.
+## Put comments here that give an overall description of what your
+## functions do
 
-## makeCacheMatrix creates a list containing a function to
-## 1. set the value of the matrix
-## 2. get the value of the matrix
-## 3. set the value of inverse of the matrix
-## 4. get the value of inverse of the matrix
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-        invM <- NULL
-        setM <- function(y) {
-                x <<- y
-                invM <<- NULL
-        }
-        getM <- function() x
-        setInv <- function(inverse) invM <<- inverse
-        getInv <- function() invM
-        list(setM = setM, getM = getM,
-             setInv = setInv,
-             getInv = getInv)
+
 }
- 
- 
-## cacheSolve will attempt to return the cached inversed matrix
-## if it exists. Otherwise, it will calculate the inverse using
-## the solve() function in R and will cache the result.
- 
+
+
+## Write a short comment describing this function
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        invM <- x$getInv()
-        if(!is.null(invM)) {
-                message("getting cached data")
-                return(invM)
-        }
-        data <- x$getM()
-        invM <- solve(data, ...)
-        x$setInv(invM)
-        invM
 }
